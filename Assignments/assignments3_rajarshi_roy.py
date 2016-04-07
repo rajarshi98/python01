4142baeb38cb17770af89ccaac8517922f0312e8
rajarshi98 <rajarshi_98@yahoo.com>
2016-04-07 19:10:44 -0400

    #3

---
+++ Assignments/assignments3_rajarshi_roy.py
@@ -0,0 +1,11 @@
+hrs = raw_input("Enter Hours:")
+h = float(hrs)
+rate = raw_input("Enter PayRate:")
+r = float(rate)
+grosspay = h * r
+if h <= 40 :
+    print grosspay
+else:
+     grosspay = r * 40 + (r * 1.5 * (h - 40))
+
+     print grosspay
---
+++ Assignments/classwork.py
@@ -0,0 +1,8 @@
+number = int(raw_input("enter a number: "))
+if number > 10:
+	print 'Greater than 10'
+elif number == 10:
+	print 'The number is 10'
+else:
+	print 'Less than 10'
+print "Your number is ",number
