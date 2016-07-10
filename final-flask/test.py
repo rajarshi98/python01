17e015a304eecb1b9e37747946a6bec9580d9697
rajarshi98 <rajarshi_98@yahoo.com>
2016-07-10 14:04:47 -0400

    flask

---
+++ final-flask/test.py
@@ -0,0 +1,18 @@
+from app import app
+import unittest
+
+class FlaskTestCase(unittest.TestCase):
+
+	# """docstring for FlaskTestCase"""
+	def test_index(self):
+		tester = app.test_client(self)
+		response = tester.get('/login', content_type='html/text')
+		self.assertEqual(response.status_code, 200)
+
+	def test_login_page_loads(self):
+		tester = app.test_client(self)
+		response = tester.get('/login', content_type='html/text')
+		self.assertTrue(b"Please login" in response.data)	
+
+if __name__	== '__main__':
+	unittest.main()
